7256861bf72f5a8cc0b51650aa24778c3f9d7af9
Nathan-Mather <njmather@umich.edu>
2019-10-30 11:33:30 -0400

    finishing WFE for now, starting PLM
    Got a basic example of WFE, Getting the Shell of the code for PLM

---
+++ Investigate packages/Understanding plm.R
@@ -0,0 +1,51 @@
+#=================================#
+# ==== Understand PLM package ====
+#=================================#
+
+  
+  # - Keywords  
+  #  - #set will be used for things that need to be set for your specific
+  #       file structure to get the code to run. Things like data directories 
+  #  - #fix will be used for things that need to be fixed 
+  #  - #note I will use the tags #note for things I think are important
+  
+  
+  
+  # - purpose of code:
+  # Get working exampels of the main funcitons in the "plm" Package 
+  # This is a huge package with a lot of little helper functions as well as several test for various 
+  # aspects of panel data. For now I am juts focusing in on the PLM function as a substitute for LM on fixed effects models 
+  # can explor other aspects of this as project evolves 
+  
+  # -requirments to run 
+  # all data necessary is from the package  
+  
+
+#=================================#
+# ==== load packages and data ====
+#=================================#
+
+
+
+
+
+#=========================#
+# ==== example plm ====
+#=========================#
+
+
+
+
+#=================================#
+# ==== exploring more options ====
+#=================================#
+
+
+# havnt done this yet but as we start developing the wrapper this may become more relevent. 
+
+
+#==========================#
+# ==== other functions ====
+#==========================#
+
+
